3ce70838b16128c6da7a8d8e3f2201cb228965c6
irakli nozadze <iraklinozadze2509@gmail.com>
2023-03-11 17:38:26 +0400

    Initial Commit

---
+++ NewsWall/NewsWallApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsWallApp.swift
+//  NewsWall
+//
+//  Created by Irakli Nozadze on 11.03.23.
+//
+
+import SwiftUI
+
+@main
+struct NewsWallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
